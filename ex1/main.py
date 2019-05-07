2bfd677b06ac5a39beae639e27dc1bd933bbcb2e
lightless233 <lightless@foxmail.com>
2019-05-07 22:19:47 +0800

    add ex1

--- ex1/main.py
+++ ex1/main.py
@@ -13,8 +13,6 @@ LEARN_ALPHA = 0.01
 def load(filename):
     """
     读取文件中的数据
-    :param filename:
-    :return:
     """
     x_data = []
     y_data = []
@@ -32,40 +30,35 @@ def load(filename):
 def compute_cost_function(m, theta, x_data, y_data):
     """
     计算代价函数 J(theta) 的值
-    :param m:
-    :param theta:
-    :param x_data:
-    :param y_data:
-    :return:
     """
-    tmp = np.dot(theta.T, x_data.T) - y_data
-    tmp = np.power(tmp, 2)
+    loss = np.dot(theta.T, x_data) - y_data
+    tmp = np.power(loss, 2)
     result = tmp.sum() / m / 2
-    # print(tmp)
-    # print(result)
     return result
 
 
-def update_theta(theta, x_data, y_data):
+def update_theta(theta, x_data, y_data, m):
     """
     批量梯度下降更新 theta 值
-    :return:
     """
 
-    xa = range(ITERATIONS)
-    ya = []
-
     for i in range(ITERATIONS):
-        tmp = np.dot(theta.T, x_data.T) - y_data
-        tmp = np.dot(tmp, x_data)
-        result = tmp.sum() * LEARN_ALPHA / len(y_data)
-        print("result: {}".format(result))
+        h = np.dot(theta.T, x_data)
+        loss = h - y_data
+        t = loss * x_data
+        delta = LEARN_ALPHA * np.mean(t, axis=1)
 
-        theta = theta - result
-        print("new theta: {}".format(theta))
-        ya.append(theta)
+        theta = theta.T - delta
+        theta = theta.T
+        # print(theta)
+        j_value = compute_cost_function(m, theta, x_data, y_data)
+        print("j value: {}".format(j_value))
 
-    # print(ya)
+    return theta
+
+
+def prediction(x, theta):
+    return x, theta[0][0] + theta[1][0] * x
 
 
 def main():
@@ -76,7 +69,7 @@ def main():
     plot.scatter(x_data, y_data)
     plot.xlabel("Population of City in 10,000s")
     plot.ylabel("Profit in $10,000s")
-    plot.show()
+    # plot.show()
 
     # theta 初始值设置为0 （2x1的矩阵）
     theta = np.zeros((2, 1))
@@ -85,9 +78,9 @@ def main():
     m = len(y_data)
     print("m: {}".format(m))
 
-    # 生成X的数组，应该为 mx2的矩阵
-    x_data = np.column_stack([np.ones((m, 1)), x_data])
-    # print("x_data: {}".format(x_data))
+    # 生成X的数组，应该为 2×m 的矩阵
+    x_data = np.row_stack([np.ones((m, 1)).T, x_data])
+    print("x_data: {}".format(x_data))
     print("x_data ndim: {}".format(x_data.shape))
 
     # 计算初始的代价函数值
@@ -95,7 +88,15 @@ def main():
     print(compute_cost_function(m, theta, x_data, y_data))
 
     # 迭代进行梯度下降
-    update_theta(theta, x_data, y_data)
+    final_theta = update_theta(theta, x_data, y_data, m)
+    print("final theta:", final_theta)
+
+    # 选取两个点绘制假设函数
+    x = np.linspace(3.5, 24)
+    y = final_theta[0][0] + final_theta[1][0] * x
+
+    plot.plot(x, y, color="red")
+    plot.show()
 
 
 if __name__ == '__main__':
